41db3422c93553d2d31c370f15b40b16796839c7
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-02 15:21:13 -0300

    altera baseURL api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,5 @@
 import _axios from 'axios';
 
 export const apirecados = _axios.create({
-  baseURL: 'https://apirecados-production.up.railway.app/',
+  baseURL: 'https://api-recados-1h9d.onrender.com/',
 });
